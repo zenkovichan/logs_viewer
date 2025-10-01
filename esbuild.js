5c1e063a7a98bea6cfb38d76b47bfdca49da2075
Zenkovich Andrei <andrei.zenkovich@playrix.com>
2025-10-01 13:55:06 +0400

    Собранный плагин

---
+++ esbuild.js
@@ -0,0 +1,33 @@
+const esbuild = require('esbuild');
+
+const production = process.argv.includes('--production');
+const watch = process.argv.includes('--watch');
+
+async function main() {
+    const ctx = await esbuild.context({
+        entryPoints: ['src/extension.ts'],
+        bundle: true,
+        format: 'cjs',
+        minify: production,
+        sourcemap: !production,
+        sourcesContent: false,
+        platform: 'node',
+        outfile: 'out/extension.js',
+        external: ['vscode'],
+        logLevel: 'info',
+    });
+    if (watch) {
+        await ctx.watch();
+    } else {
+        await ctx.rebuild();
+        await ctx.dispose();
+    }
+}
+
+main().catch(e => {
+    console.error(e);
+    process.exit(1);
+});
+
+
+
